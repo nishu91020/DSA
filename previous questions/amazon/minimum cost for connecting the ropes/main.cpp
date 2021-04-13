128c4701d7277a4b726b63e8d356a15dd5f45a08
Nishu Rai <rainishu111@gmail.com>
2021-04-13 23:04:35 +0530

    minimum cost for joining rope and spiral traversal of tree

---
+++ previous questions/amazon/minimum cost for connecting the ropes/main.cpp
@@ -0,0 +1,64 @@
+/*There are given N ropes of different lengths, we need to connect these ropes into one rope.
+The cost to connect two ropes is equal to sum of their lengths. The task is to connect the ropes with minimum cost.
+
+Example 1:
+
+Input:
+n = 4
+arr[] = {4, 3, 2, 6}
+Output:
+29
+Explanation:
+For example if we are given 4
+ropes of lengths 4, 3, 2 and 6. We can
+connect the ropes in following ways.
+1) First connect ropes of lengths 2 and 3.
+Now we have three ropes of lengths 4, 6
+and 5.
+2) Now connect ropes of lengths 4 and 5.
+Now we have two ropes of lengths 6 and 9.
+3) Finally connect the two ropes and all
+ropes have connected.
+Total cost for connecting all ropes is 5
++ 9 + 15 = 29. This is the optimized cost
+for connecting ropes. Other ways of
+connecting ropes would always have same
+or more cost. For example, if we connect
+4 and 6 first (we get three strings of 3,
+2 and 10), then connect 10 and 3 (we get
+two strings of 13 and 2). Finally we
+connect 13 and 2. Total cost in this way
+is 10 + 13 + 15 = 38.
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+int find_cost(int a[],int n){
+    priority_queue<int,vector<int>, greater<int> >min_heap;
+    for(int i=0;i<n;i++)
+        min_heap.push(a[i]);
+    int sum=0;
+    while(min_heap.size()>1)
+    {
+        int first=min_heap.top();
+        min_heap.pop();
+        int second=min_heap.top();
+        min_heap.pop();
+        sum+=first+second;
+        min_heap.push(first+second);
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int length[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>length[i];
+    }
+    int cost=find_cost(length,n);
+    cout<<"total cost="<<cost<<"\n";
+    return 0;
+}
